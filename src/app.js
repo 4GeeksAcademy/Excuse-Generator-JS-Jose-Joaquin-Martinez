8ae47982532c03e54e630e0a07469fbb202751ed
JoseJoaquinMartinez <61237112+JoseJoaquinMartinez@users.noreply.github.com>
2024-02-02 17:53:25 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,35 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+function excuseGenerator(who, action, what, when) {
+  return (
+    who[Math.floor(Math.random() * who.length)] +
+    " " +
+    action[Math.floor(Math.random() * action.length)] +
+    " " +
+    what[Math.floor(Math.random() * what.length)] +
+    " " +
+    when[Math.floor(Math.random() * when.length)]
+  );
+}
+
+function onLoad() {
+  document.getElementById("excuse").innerHTML = excuseGenerator(
+    who,
+    action,
+    what,
+    when
+  );
+}
+window.onload = onLoad;
